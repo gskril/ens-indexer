ed10152a308c1ab89ea634343b7ac5fe5bb9c796
Greg Skriloff <35093316+gskril@users.noreply.github.com>
2024-04-26 17:03:10 -0400

    Handle all relevant events from the Registry

--- src/Registry.ts
+++ src/Registry.ts
@@ -1,5 +1,22 @@
 import { ponder } from '@/generated'
 
+// Logged when the owner of a node transfers ownership to a new account.
+ponder.on('Registry:Transfer', async ({ event, context }) => {
+  const { Name } = context.db
+  const { node, owner } = event.args
+
+  await Name.upsert({
+    id: node,
+    create: {
+      owner,
+    },
+    update: {
+      owner,
+    },
+  })
+})
+
+// Logged when the owner of a node assigns a new owner to a subnode.
 ponder.on('Registry:NewOwner', async ({ event, context }) => {
   const { Name } = context.db
   const { node, label, owner } = event.args
@@ -16,6 +33,7 @@ ponder.on('Registry:NewOwner', async ({ event, context }) => {
   })
 })
 
+// Logged when the resolver for a node changes.
 ponder.on('Registry:NewResolver', async ({ event, context }) => {
   const { Name } = context.db
   const { node, resolver } = event.args
