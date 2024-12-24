1991aa8e2ef87f980bbadc1fc53bc2eac15040a4
Greg Skriloff <35093316+gskril@users.noreply.github.com>
2024-12-23 22:17:11 -0500

    Handle invalid .eth names

--- src/RegistrarControllerV2.ts
+++ src/RegistrarControllerV2.ts
@@ -1,20 +1,22 @@
 import { ponder } from '@/generated'
-import { namehash } from 'viem'
+import { keccak256, namehash, toHex } from 'viem'
 
 import { name } from '../ponder.schema'
+import { getNodeFromParentNodeAndLabelhash } from './utils'
 
 ponder.on(
   'RegistrarControllerV2:NameRegistered',
   async ({ event, context }) => {
     const { name: label } = event.args
 
-    const nameWithTld = `${label}.eth`
-    const node = namehash(nameWithTld)
+    const parentNode = namehash('eth')
+    const labelhash = keccak256(toHex(label))
+    const node = getNodeFromParentNodeAndLabelhash(parentNode, labelhash)
 
     // We can ignore the `owner` and `expires` fields since they are already tracked by the BaseRegistrar
     await context.db
       .update(name, { id: node })
-      .set(() => ({ name: nameWithTld, label }))
+      .set(() => ({ name: `${label}.eth`, label }))
   }
 )
 
